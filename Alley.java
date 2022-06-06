53b27bdd15f1b40a375d3231f80db88aa44a11c1
335744314 <99493761+335744314@users.noreply.github.com>
2022-06-06 10:53:06 -0400

    added hole and improved wall bouncing

--- Alley.java
+++ Alley.java
@@ -1,36 +1,45 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * This class is the random game world. Will randomize objects
  * 
- * @author (your name) 
+ * @KT
  * @version (a version number or a date)
  */
 public class Alley extends World
 {
     private MouseInfo mouse; 
     private Circle object = new Circle();
+    private Hole hole = new Hole();
     
     private double angleX;
     private double angleY;
     private double speed;
     private double rotation;
     private int height = 0;
+    private int strokes;
     
     private boolean startDrag = false;
-    private static boolean released = false;
+    private boolean released = false;
     private boolean clicked = false;
+    private static boolean reset = true;
     
     private Label check;
     private Wall[] walls;
     
     public Alley()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world
         super(1100, 500, 1); 
+        // Initializes the world by adding all necessary objects and variables
+        reset = true;
+        strokes = 0;
         addObject(object, 200, 250);
         check = new Label(angleX + ", " + angleY, 50);
         addObject(check, 650, 50);
+        addObject(hole, 900, 250);
+        
+        // Randomizes the number of walls and adds them at random locations
         walls = new Wall[Greenfoot.getRandomNumber(7) + 4];
         for(int i = 0; i < walls.length; i++)
         {
@@ -42,9 +51,14 @@ public class Alley extends World
     
     public void act()
     {
+        /* The following code is used to calculate the launch
+         * angle and speed of the ball relative to the user's mouse.
+         * It can only be activated when the ball is not moving and when 
+         * the mouse is in the world.
+         */
         mouse = Greenfoot.getMouseInfo();
 
-        if(mouse != null && mouse.getActor() == object && mouse.getButton() == 1 && !released)
+        if(mouse != null && mouse.getActor() == object && mouse.getButton() == 1 && !released && reset)
         {
             startDrag = true;
         }
@@ -76,11 +90,19 @@ public class Alley extends World
             }
             object.startMoving(speed, rotation);
             released = false;
+            reset = false;
+            strokes++;
+        }
+        
+        // If the ball lands in the hole, the game ends and the player wins
+        if(Circle.win)
+        {
+            Greenfoot.setWorld(new EndScreen(strokes));
         }
     }
     
-    public static void resetReleased()
+    public static void reset()
     {
-        released = false;
+        reset = true;
     }
 }
--- Circle.java
+++ Circle.java
@@ -16,9 +16,11 @@ public class Circle extends SmoothMover
     private boolean move = false;
     private Actor curWall;
     
+    public static boolean win = false;
+    
     public Circle()
     {
-
+        win = false;
     }
     
     public void act() 
@@ -36,22 +38,22 @@ public class Circle extends SmoothMover
             if(isTouching(Wall.class))
             {
                 curWall = getOneIntersectingObject(Wall.class);
-                if(edgeX + 10 >= curWall.getX() - 20 && edgeX <= curWall.getX() - 15 && edgeY < curWall.getY() + 25 && edgeY> curWall.getY() - 25)
+                if(edgeX + 10 >= curWall.getX() - 20 && edgeX <= curWall.getX() - 15)
                 {
                     rotation = 180 - rotation;
                     setRotation(rotation);
                 }
-                if(edgeX - 10 <= curWall.getX() + 20 && edgeX >= curWall.getX() + 15 && edgeY < curWall.getY() + 25 && edgeY> curWall.getY() - 25)
+                if(edgeX - 10 <= curWall.getX() + 20 && edgeX >= curWall.getX() + 15)
                 {
                     rotation = 180 - rotation;
                     setRotation(180 - rotation);
                 }
-                if(edgeY + 10 >= curWall.getY() - 25 && edgeY <= curWall.getY() - 20 && edgeX < curWall.getX() + 20 && edgeX > curWall.getX() - 20 )
+                if(edgeY + 10 >= curWall.getY() - 25 && edgeY <= curWall.getY() - 20)
                 {
                     rotation = -rotation;
                     setRotation(rotation);
                 }
-                if(edgeY - 10 <= curWall.getY() + 25 && edgeY >= curWall.getY() + 20 && edgeX < curWall.getX() + 20 && edgeX > curWall.getX() - 20 )
+                if(edgeY - 10 <= curWall.getY() + 25 && edgeY >= curWall.getY() + 20)
                 {
                     rotation = -rotation;
                     setRotation(rotation);
@@ -63,21 +65,39 @@ public class Circle extends SmoothMover
             {
                 rotation = 180 - rotation;
                 setRotation(rotation);
+                setLocation(1089, getExactY());
             }
             if(edgeX - 10 <= 0)
             {
                 rotation = 180 - rotation;
                 setRotation(rotation);
+                setLocation(11, getExactY());
             }
             if(edgeY + 10 >= 500)
             {
                 rotation = -rotation;
                 setRotation(rotation);
+                setLocation(getExactX(), 489);
             }
             if(edgeY - 10 <= 0)
             {
                 rotation = -rotation;
                 setRotation(rotation);
+                setLocation(getExactX(), 11);
+            }
+            
+            // Collision with hole - goes in hole at low enough speeds
+            if(isTouching(Hole.class))
+            {
+                if(speed > 3.5)
+                {
+                    setRotation(Greenfoot.getRandomNumber(360));
+                }
+                else
+                {
+                    getWorld().removeObject(this);
+                    win = true;
+                }
             }
             
             // Checks whether the ball has stopped or not. If it has stopped, the ball will no longer move and
@@ -85,7 +105,7 @@ public class Circle extends SmoothMover
             if(speed <= 0)
             {
                 move = false;
-                Alley.resetReleased();
+                Alley.reset();
             }
         }
     }  
